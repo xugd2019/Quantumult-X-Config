0b3cbc109c1acfd52aac4dd22f301d0724301859
xugd2019 <65591330+xugd2019@users.noreply.github.com>
2022-05-25 01:00:57 +0800

    Create Naruto3.js

---
+++ scripts/Naruto3.js
@@ -0,0 +1,35 @@
+
+/**
+ * 验证3
+ * 
+ */
+
+const url = "https://gw.gzh.qq.com/sidip/site/role/display?site=hyrz&login_type=wx";
+const method = "GET";
+const headers = {
+"Origin" : "https://gzhcos.qq.com",
+"Cookie" : "fsza_sk_s_q_101482157=1653409211; fsza_sk_s_q_at_101482157=1653409211; fsza_sk_t_q_101482157=01G3VDWS91VJQKWJ9STFQWJ1HY; fsza_sk_t_q_at_101482157=01G3VDWS91VJQKWJ9STFQWJ1HY; gzh_user_status=29cOxr5jOFgPOe1ACH2WPqv05IE; fsza_sk_s_at_wxa817069bb040f860=1653409155; fsza_sk_t_at_wxa817069bb040f860=0eb432559436a7dc0be8cb28ba83f3ecb874cd626f8e520efee58c9f5b5ff966",
+"Connection" : "keep-alive",
+"Accept" : "*/*",
+"Referer" : "https://gzhcos.qq.com/",
+"Host" : "gw.gzh.qq.com",
+"User-Agent" : "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.22(0x18001624) NetType/WIFI Language/zh_CN"""""",
+"Accept-Encoding" : "gzip, deflate, br",
+"Accept-Language" : "zh-CN,zh-Hans;q=0.9"
+};
+const body = "";
+
+const myRequest = {
+    url: url,
+    method: method,
+    headers: headers,
+    body: body
+};
+
+$task.fetch(myRequest).then(response => {
+    console.log(response.statusCode + "\n\n" + response.body);
+    $done();
+}, reason => {
+    console.log(reason.error);
+    $done();
+});
