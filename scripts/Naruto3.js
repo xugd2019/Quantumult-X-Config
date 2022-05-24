d91f120915682930cf38c125cb1484f8f2675fe5
xugd2019 <65591330+xugd2019@users.noreply.github.com>
2022-05-25 01:15:37 +0800

    Update Naruto3.js

--- scripts/Naruto3.js
+++ scripts/Naruto3.js
@@ -1,23 +1,23 @@
 
 /**
- * 验证3
+ * @fileoverview Template to compose HTTP reqeuest.
  * 
  */
 
-const url = "https://gw.gzh.qq.com/sidip/site/role/display?site=hyrz&login_type=wx";
-const method = "GET";
+const url = `https://gw.gzh.qq.com/sidip/site/role/display?site=hyrz&login_type=wx`;
+const method = `GET`;
 const headers = {
-"Origin" : "https://gzhcos.qq.com",
-"Cookie" : $prefs.valueForKey("cookie_naruto"),
-"Connection" : "keep-alive",
-"Accept" : "*/*",
-"Referer" : "https://gzhcos.qq.com/",
-"Host" : "gw.gzh.qq.com",
-"User-Agent" : "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.22(0x18001624) NetType/WIFI Language/zh_CN"""""",
-"Accept-Encoding" : "gzip, deflate, br",
-"Accept-Language" : "zh-CN,zh-Hans;q=0.9"
+'Origin' : `https://gzhcos.qq.com`,
+'Cookie' : ,
+'Connection' : `keep-alive`,
+'Accept' : `*/*`,
+'Referer' : `https://gzhcos.qq.com/`,
+'Host' : `gw.gzh.qq.com`,
+'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.22(0x18001624) NetType/WIFI Language/zh_CN`,
+'Accept-Encoding' : `gzip, deflate, br`,
+'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
-const body = "";
+const body = ``;
 
 const myRequest = {
     url: url,
