1924bcd2ab56e1de858cda3a24a8911f316323b6
xugd2019 <65591330+xugd2019@users.noreply.github.com>
2022-05-25 01:19:12 +0800

    Update NarutoOne.js

--- scripts/NarutoOne.js
+++ scripts/NarutoOne.js
@@ -1,20 +1,22 @@
+
 /**
- * 验证1
+ * @fileoverview Template to compose HTTP reqeuest.
+ * 
  */
 
-const url = "https://apps.gzh.qq.com/common/wx/api/thirdpartySign.php?sAppid=wxa817069bb040f860&sUrl=https%3A%2F%2Fgzhcos.qq.com%2Fawp-activity%2Fcommon%2F%3FADTAG%3Dhyrz.15&callback=__jp0";
-const method = "GET";
+const url = `https://apps.gzh.qq.com/common/wx/api/thirdpartySign.php?sAppid=wxa817069bb040f860&sUrl=https%3A%2F%2Fgzhcos.qq.com%2Fawp-activity%2Fcommon%2F%3FADTAG%3Dhyrz.15&callback=__jp0`;
+const method = `GET`;
 const headers = {
-"Cookie" : $prefs.valueForKey("cookie_naruto"),
-"Accept" : "*/*",
-"Connection' : "keep-alive",
-"Referer" : "https://gzhcos.qq.com/",
-"Accept-Encoding" : "gzip, deflate, br",
-"Host" : "apps.gzh.qq.com",
-"User-Agent" : "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.22(0x18001624) NetType/WIFI Language/zh_CN",
-"Accept-Language" : "zh-CN,zh-Hans;q=0.9"
+'Cookie' : $prefs.valueForKey("cookie_naruto"),
+'Accept' : `*/*`,
+'Connection' : `keep-alive`,
+'Referer' : `https://gzhcos.qq.com/`,
+'Accept-Encoding' : `gzip, deflate, br`,
+'Host' : `apps.gzh.qq.com`,
+'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.22(0x18001624) NetType/WIFI Language/zh_CN`,
+'Accept-Language' : `zh-CN,zh-Hans;q=0.9`
 };
-const body = "";
+const body = ``;
 
 const myRequest = {
     url: url,
